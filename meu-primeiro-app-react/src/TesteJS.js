fa655558b6aa15dd72cea937e0bdee461816e39b
Yasmin Feliciano <713yasminlima@gmail.com>
2026-04-01 16:06:57 -0300

    Criando meu primeiro app react

---
+++ meu-primeiro-app-react/src/TesteJS.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const App = () => { 
+return( 
+<div style={{margin: 'auto', width: 768, backgroundColor: '#EEE', padding: 
+12, borderRadius: 8}}> 
+<label htmlFor="nome" style={{display: 'block', marginBottom: 
+4}}>Nome:</label> 
+<input type="text" id="nome" style={{paddingTop: 8, paddingBottom: 8, 
+borderStyle: 'hidden',width: '100%', borderRadius: 8, outline: 'none', boxSizing: 
+'border-box'}}/> 
+<button style={{marginTop: 12, paddingTop: 8, paddingBottom: 8, 
+backgroundColor: 'blueviolet', color: 'white', border: 'none', width: '100%', 
+borderRadius: 8}}>Enviar</button> 
+</div> 
+) 
+} 
+
+const root = ReactDOM.createRoot(document.querySelector("#root"))
+
+root.render(<App />)
---
+++ meu-primeiro-app-react/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './styles.css' 
+ 
+const App = () => { 
+    const estilosBotao = {marginTop: 12, paddingTop: 8, paddingBottom: 8, 
+backgroundColor: 'blueviolet', color: 'white', border: 'none', width: "100%", 
+borderRadius: 8}; 
+    const textoDoRotulo = "Nome:"; 
+    const obterTextoDoBotao = () => { 
+        return "Enviar"; 
+    } 
+    return( 
+        <div style={{margin: 'auto', width: 768, backgroundColor: '#EEE', padding: 
+12, borderRadius: 8}}> 
+            <label className="rotulo" htmlFor="nome" style={{display: 'block', 
+marginBottom: 4}}>{textoDoRotulo}</label> 
+            <input type="text" id="nome" style={{paddingTop: 8, paddingBottom: 8, 
+borderStyle: 'hidden',width: '100%', borderRadius: 8, outline: 'none', boxSizing: 
+'border-box'}}/> 
+            <button style={estilosBotao}>{obterTextoDoBotao()}</button> 
+        </div> 
+    ) 
+} 
+
+const root = ReactDOM.createRoot(document.querySelector("#root"))
+
+root.render(<App />)
